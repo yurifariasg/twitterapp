af9bffc1688b0870c681f10decb52ab1c43e6668
Yuri Gomes <yuri.gomes@adidas.com>
2017-03-01 10:52:09 +0100

    Updated fastlane

---
+++ Twitter App/Twitter App/Tests/TwitterTests.swift
@@ -0,0 +1,39 @@
+//
+//  TwitterTests.swift
+//  Twitter App
+//
+//  Created by Gomes, Yuri (formerly Farias Gomes) on 01/03/17.
+//  Copyright © 2017 Gomes, Yuri (formerly Farias Gomes). All rights reserved.
+//
+
+import XCTest
+import UIKit
+import Pods_Twitter_App
+
+
+class TwitterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
